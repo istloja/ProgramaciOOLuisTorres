95624e69d64156139fa0c99cc8f98ea7a2bee28f
DELL <DELL@DESKTOP-C0LCSBF>
2019-07-16 18:57:19 -0500

    sincronizando el ejercicio

--- trabajo_clase/src/trabajo_clase/Ejecutar.java
+++ trabajo_clase/src/trabajo_clase/Ejecutar.java
@@ -14,6 +14,10 @@ public class Ejecutar {
         Animal  objetoanimal=new Animal("perro" ,20,50,"blanco", 6 );
         Ave objeto2=new Ave(5, true, "Aguila",2.56,30,"rojo",5);
         Mamifero objeto3= new Mamifero("coki",10.5,30,"cafe",13);
+        objetoanimal.alimentarse();
+        objeto2.alimentarse();
+        objeto3.alimentarse();
+        objetoanimal.informacionAnimal();
     }
  
 }
